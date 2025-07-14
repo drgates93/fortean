0935709413ef43d444e9c2e4657fcf1ea0419e17
drgates93 <dgates8@gmail.com>
2025-07-14 10:48:52 -0500

    Delete fortean directory

--- fortean/src/fortean_build.h
+++
@@ -1,6 +0,0 @@
-#ifndef FORTEAN_BUILD_H
-#define FORTEAN_BUILD_H
-
-int fortean_build_project_incremental(const int parallel_build, const int incremental_build_override, const int lib_only);
-
-#endif // FORTEAN_BUILD_H
